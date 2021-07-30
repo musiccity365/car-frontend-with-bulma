ce76eadd224ddc9d3fc0f8a55116b5f9fc4eb82c
musiccity365 <m_sackstein@yahoo.com>
2021-07-29 22:50:08 -0400

    adding event listeners

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/cars"
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(endPoint)
+        .then(response => response.json())
+        .then(cars => {
+            console.log(cars);
+        })
+})
