d71959702328bbc210d98fa750d27c12876f152f
musiccity365 <m_sackstein@yahoo.com>
2021-08-10 00:20:48 -0400

    refactored code (POST, fetch, render, etc.)

--- index.js
+++ index.js
@@ -13,20 +13,50 @@ function getCars() {
         .then(response => response.json())
         .then(cars => {
             cars.data.forEach(car => {
-                const carMarkup = `
-                <div data-id=${car.id}>
-                    <img src=${car.attributes.image_url}
-                    height="200" width="200">
-                    <h3>${car.attributes.year} ${car.attributes.make} ${car.attributes.model}</h3>
-                    <img src=${car.attributes.image_url} height="200" width="250">
-                    <p>${car.attributes.origin.name}</p>
-                    <button data-id=${car.id}>edit</button>
-                </div>
-                <br><br>`;
-                // use data-id instead of id to enable the functionality of the button to be altered at a later time if necessary.
-                document.querySelector('#car-container').innerHTML += carMarkup;
+                render(car);
             });
         });
+}
+
+// POST request for cars
+function postFetch(year, make, model, image_url, originId) {
+    //build body outside of fetch request
+    const bodyData = {
+        year,
+        make,
+        model,
+        image_url,
+        originId
+    }
+    fetch(endPoint, {
+            // POST request
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(bodyData)
+        })
+        .then(response => response.json())
+        // .catch(err => console.log(err))
+        .then(car => {
+            console.log(car);
+            const carData = car.data;
+            //render JSON response
+            render(carData)
+        })
+}
+
+function render(car) {
+    const carMarkup = `
+    <div data-id=${car.id}>
+        <img src=${car.attributes.image_url}
+        height="200" width="200">
+        <h3>${car.attributes.year} ${car.attributes.make} ${car.attributes.model}</h3>
+        <img src=${car.attributes.image_url} height="200" width="250">
+        <p>${car.attributes.origin.name}</p>
+        <button data-id=${car.id}>edit</button>
+    </div>
+    <br><br>`;
+    // use data-id instead of id to enable the functionality of the button to be altered at a later time if necessary.
+    document.querySelector('#car-container').innerHTML += carMarkup;
 }
 
 function createFormHandler(e) {
@@ -37,8 +67,4 @@ function createFormHandler(e) {
     const imageInput = document.querySelector('#input-url').value;
     const originId = parseInt(document.querySelector('#origins').value);
     postFetch(yearInput, makeInput, modelInput, imageInput, originId);
-}
-
-function postFetch(year, make, model, image_url, originId) {
-    console.log(year, make, model, image_url, originId);
 }
